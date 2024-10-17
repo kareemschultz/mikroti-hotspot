7efc38f25ab47c11557fc001101f7bd3e799d3c0
Kareem Schultz <75060872+kareemschultz@users.noreply.github.com>
2024-10-17 15:39:01 -0400

    Update DatabaseSeeder.php

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,14 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,18 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        Permission::create(['name' =>  'admin-function']);
-
-        $adminUser = Role::create(['name' => 'admin']);
-
-        $adminUser->givePermissionTo([
-            'admin-function',
-        ]);
-
+        // User creation
         $admin = User::create([
             'name'          =>  'Administrator',
-            'username'         =>  'admin',
+            'username'      =>  'admin',
             'password'      =>  Hash::make('admin@1234'),
             'api_public'    =>  Str::random(12),
             'api_secret'    =>  Str::random(12),
@@ -38,14 +27,12 @@ class DatabaseSeeder extends Seeder
             'mobile'        =>  '09123456789',
         ]);
 
-        $admin->assignRole('admin');
-
+        // App settings
         DB::table('app_settings')->insert([
             ['name' =>  'APP_NAME',     'value' =>  'Mendyfi'],
             ['name' =>  'INRO_TEXT',    'value' =>  'Centalize Hotspot System'],
             ['name' =>  'SUB_TEXT',     'value' =>  'Radius for your Hotspot'],
             ['name' =>  'RADIUS_INTERIM', 'value' => '1'],
         ]);
-
     }
 }
